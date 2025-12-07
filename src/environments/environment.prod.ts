0ca92cd381c36a502e7b96b686e157d56906fae8
jipheens <wahomejipheens@gmail.com>
2025-12-07 14:18:04 +0300

    bbccbbb

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -55,6 +55,8 @@ export const environment = {
   clientUrlHR: "http://197.155.71.138:6301/administration",
   selfServiceUrlHR: "http://197.155.71.138:6301/portal",
   branchUrl:"http://localhost:5177",
-  clintUrl:"http://172.17.50.10:6902"
+  clintUrl:"http://172.17.50.10:6902",
+    branachUrl:"http://localhost:5177",
+  clicntUrl:"http://172.17.50.10:6902"
 
 };
